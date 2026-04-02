3b9b3444a34777ae81cb5ee968f673f63ea9e475
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-04-02 21:59:29 +0530

    fix: remove unnecessary npm line from App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,4 @@ import { router } from "./routes";
 export default function App() {
   return <RouterProvider router={router} />;
 }
-<<<<<<< HEAD
-=======
-npm 
->>>>>>> 2d04cf749f7f1e7780b47c54faf4831c4f3cda36
+
